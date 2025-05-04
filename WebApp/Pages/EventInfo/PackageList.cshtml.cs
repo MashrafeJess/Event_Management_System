144059b272c6ae8039703c79dda13f973ed62818
MashrafeJess <mashrafeiubat@gmail.com>
2025-05-05 00:30:45 +0600

    Not Last

---
+++ WebApp/Pages/EventInfo/PackageList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.EventInfo
+{
+    public class PackageListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
